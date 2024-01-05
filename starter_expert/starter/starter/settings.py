e9820bb9d4b794ef6f5268f2ae346c9eba04e942
seeski abadzekh <skiba.bogdan.work@gmail.com>
2024-01-05 13:35:16 +0300

    db host changed

--- starter_expert/starter/starter/settings.py
+++ starter_expert/starter/starter/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'starter.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        'HOST': 'starterdb',
+        'HOST': '0.0.0.0',
         # 'HOST': '127.0.0.1',
         'PORT': '5432',
         'USER': 'seeski',
